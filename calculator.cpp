e0df9a98b2bfd90d6e8c500b308685ae8e88eed1
Vibish <vibish246@gmail.com>
2026-07-15 14:17:34 +0530

    more functions added (sub,mult,divide,rem)

--- calculator.cpp
+++ calculator.cpp
@@ -4,6 +4,18 @@ using namespace std;
 int add(int a,int b) {
     return a+b;
 }
+int sub(int a,int b) {
+    return a-b;
+}
+int mult(int a,int b) {
+    return a*b;
+}
+int divide(int a,int b) {
+    return a/b;
+}
+int rem(int a,int b) {
+    return a%b;
+}
 int main() {
 
 }
